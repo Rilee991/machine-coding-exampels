a9e8d3bfa07be04016b562902eac2ce916646bc2
cypher33 <rohitkumar25.rkl@gmail.com>
2024-01-07 07:43:51 +0530

    autocomplete using vanilla js 2

--- autocomplete-vanillajs/index.js
+++ autocomplete-vanillajs/index.js
@@ -42,66 +42,125 @@ function getSuggestions(text) {
 // getSuggestions(text); method will act as mock server and will return random text based on the inputs with 0 - 200 millisceond latency and may fail.
 // if a suggestion is clicked, populate the input box with its value and bring input box in focus
 
-(function(){
-  const input = document.getElementById("search");
-  const suggestionArea = document.getElementById("suggestion-area");
+// (function(){
+//   const input = document.getElementById("search");
+//   const suggestionArea = document.getElementById("suggestion-area");
   
-  const onFocus = () => {
-    suggestionArea.style.display = "block";
-  }
+//   const onFocus = () => {
+//     suggestionArea.style.display = "block";
+//   }
   
-  const onBlur = (e) => {
-    if(e.target === input || e.target === suggestionArea){
-      return;
-    }
+//   const onBlur = (e) => {
+//     if(e.target === input || e.target === suggestionArea){
+//       return;
+//     }
     
-    suggestionArea.style.display = "none";
-  }
+//     suggestionArea.style.display = "none";
+//   }
   
-  const onChange = (e) => {
-    const {value} = e.target;
-    processData(value);
-  }
+//   const onChange = (e) => {
+//     const {value} = e.target;
+//     processData(value);
+//   }
   
-  const processData = async (value) => {
-    suggestionArea.style.display = "block";
-    suggestionArea.innerHTML = "";
+//   const processData = async (value) => {
+//     suggestionArea.style.display = "block";
+//     suggestionArea.innerHTML = "";
     
-    if(!value){
-      return;
-    }
+//     if(!value){
+//       return;
+//     }
     
-    try{
-      const resp = await getSuggestions(value);
-      if(resp.length > 0){
-        const list = document.createElement('ul');
-        resp.forEach((e) => {
-          const listItems = document.createElement('li');
-          listItems.style.cursor = "pointer";
-          listItems.innerText = e;
-          list.appendChild(listItems);
-        });
+//     try{
+//       const resp = await getSuggestions(value);
+//       if(resp.length > 0){
+//         const list = document.createElement('ul');
+//         resp.forEach((e) => {
+//           const listItems = document.createElement('li');
+//           listItems.style.cursor = "pointer";
+//           listItems.innerText = e;
+//           list.appendChild(listItems);
+//         });
         
-        suggestionArea.innerHTML = "";
-        suggestionArea.appendChild(list);
-      }
-    }catch(e){
-      console.error("Error while making network call", e);
-    }
-  } 
+//         suggestionArea.innerHTML = "";
+//         suggestionArea.appendChild(list);
+//       }
+//     }catch(e){
+//       console.error("Error while making network call", e);
+//     }
+//   } 
   
-  const onClick = (e) => {
-    if(e.target === suggestionArea){
-      return;
+//   const onClick = (e) => {
+//     if(e.target === suggestionArea){
+//       return;
+//     }
+    
+//     const text = e.target.innerText;
+//     input.value = text;
+//     input.focus();
+//   }
+  
+//   input.addEventListener('focus', onFocus);
+//   window.addEventListener('click', onBlur);
+//   input.addEventListener('keyup', onChange);
+//   suggestionArea.addEventListener('click', onClick, true);
+// }());
+
+(function () {
+    const input = document.getElementById("search");
+    const suggestionArea = document.getElementById("suggestion-area");
+    
+    const onFocus = () => {
+        suggestionArea.style.display = 'block';
     }
     
-    const text = e.target.innerText;
-    input.value = text;
-    input.focus();
-  }
-  
-  input.addEventListener('focus', onFocus);
-  window.addEventListener('click', onBlur);
-  input.addEventListener('keyup', onChange);
-  suggestionArea.addEventListener('click', onClick, true);
+    const onBlur = (e) => {
+        if(e.target == input || e.target == suggestionArea)
+            return;
+        suggestionArea.style.display = 'none';
+    }
+
+    const onClick = (e) => {
+        suggestionArea.style.display = 'block';
+
+        if(e.target == suggestionArea) return;
+
+        const text = e.target.innerText;
+        input.value = text;
+        input.focus();
+    }
+
+    const onKeyUp = (e) => {
+        suggestionArea.style.display = "block";
+        suggestionArea.innerHTML = "";
+        processData(e.target.value);
+    }
+
+    const processData = async (text) => {
+        if(!text) {
+            return;
+        }
+
+        try {
+            const resp = await getSuggestions(text);
+            if(resp.length > 0) {
+                const list = document.createElement('ul');
+                resp.forEach(res => {
+                    const item = document.createElement('li');
+                    item.innerText = res;
+                    item.style.cursor = 'pointer';
+                    list.appendChild(item);
+                });
+                suggestionArea.innerHTML = "";
+                suggestionArea.appendChild(list);
+            }
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+
+    input.addEventListener("focus", onFocus);
+    window.addEventListener("click", onBlur);
+    input.addEventListener("keyup", onKeyUp);
+    suggestionArea.addEventListener("click", onClick, true);
 }());
